f4363bbb11e650a57451f96b02aa708e00a8a7f8
Konstantin Läufer <laufer@cs.luc.edu>
2012-09-24 16:09:30 -0500

    combined main and test into single repo

---
+++ test/README.md
@@ -0,0 +1,16 @@
+# Learning Objectives
+
+* Automated unit and integration testing with JUnit
+* Testcase Superclass pattern for xUnit testing
+* Simple dependency injection
+* Automated system testing by interacting with the GUI
+* Automated GUI testing in Android
+
+# Running the Example
+
+To run the tests through Eclipse, make sure you have both this test
+project and the subject-under-test (original project) open in the
+current workspace.
+
+* Non-Android test of the bounded counter: right-click on `src/edu...model` > Run As... > JUnit Test
+* Android test of the activity: right-click on the project root > Run As... > Android JUnit Test
---
+++ test/src/edu/luc/etl/cs313/android/clickcounter/ui/ClickCounterControllerTest.java
@@ -0,0 +1,85 @@
+package edu.luc.etl.cs313.android.clickcounter.ui;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
+import android.widget.Button;
+import android.widget.TextView;
+
+import edu.luc.etl.cs313.android.clickcounter.R;
+
+/**
+ * GUI-level test of several essential click-counter scenarios.
+ * 
+ * @author laufer
+ * @see http://developer.android.com/tools/testing/activity_testing.html
+ */
+public class ClickCounterControllerTest extends ActivityInstrumentationTestCase2<ClickCounterController> {
+
+    /**
+     * Creates an {@link ActivityInstrumentationTestCase2} for the {@link SkeletonActivity}
+     * activity.
+     */
+	public ClickCounterControllerTest() {
+		super(ClickCounterController.class);
+	}
+	
+    /**
+     * Verifies that the activity under test can be launched.
+     */
+    public void testActivityTestCaseSetUpProperly() {
+        assertNotNull("activity should be launched successfully", getActivity());
+    }	
+
+    @UiThreadTest
+    public void testActivityScenarioIncReset() {
+    	assertEquals(0, getDisplayedValue());
+    	assertTrue(getIncButton().isEnabled());
+    	assertFalse(getDecButton().isEnabled());
+    	assertTrue(getResetButton().isEnabled());
+    	assertTrue(getIncButton().performClick());
+    	assertEquals(1, getDisplayedValue());
+    	assertTrue(getIncButton().isEnabled());
+    	assertTrue(getDecButton().isEnabled());
+    	assertTrue(getResetButton().isEnabled());
+    	assertTrue(getResetButton().performClick());
+    	assertEquals(0, getDisplayedValue());
+    	assertTrue(getIncButton().isEnabled());
+    	assertFalse(getDecButton().isEnabled());
+    	assertTrue(getResetButton().isEnabled());
+    }
+    
+    @UiThreadTest
+    public void testActivityScenarioIncUntilFull() {
+    	assertEquals(0, getDisplayedValue());
+    	assertTrue(getIncButton().isEnabled());
+    	assertFalse(getDecButton().isEnabled());
+    	assertTrue(getResetButton().isEnabled());
+    	while (getIncButton().isEnabled()) {
+    		final int v = getDisplayedValue();
+        	assertTrue(getIncButton().performClick());
+        	assertEquals(v + 1, getDisplayedValue());
+    	}
+    	assertFalse(getIncButton().isEnabled());
+    	assertTrue(getDecButton().isEnabled());
+    	assertTrue(getResetButton().isEnabled());
+    }
+   
+    // auxiliary methods for easy access to UI widgets
+    
+    protected int getDisplayedValue() {
+    	final TextView t = (TextView) getActivity().findViewById(R.id.textview_value);
+    	return Integer.parseInt(t.getText().toString().trim());
+    }
+    
+    protected Button getIncButton() {
+    	return (Button) getActivity().findViewById(R.id.button_increment);
+    }
+    
+    protected Button getDecButton() {
+    	return (Button) getActivity().findViewById(R.id.button_decrement);
+    }
+
+    protected Button getResetButton() {
+    	return (Button) getActivity().findViewById(R.id.button_reset);
+    }
+}
