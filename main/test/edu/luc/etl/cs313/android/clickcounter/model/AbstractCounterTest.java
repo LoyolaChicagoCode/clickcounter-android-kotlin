bf79c9b810589325881547b37dd22a061da8c3b9
Konstantin Läufer <laufer@cs.luc.edu>
2013-01-31 19:36:18 -0600

    moved model changes to main project; changed dependencies to Eclipse user libraries

---
+++ main/test/edu/luc/etl/cs313/android/clickcounter/model/AbstractCounterTest.java
@@ -0,0 +1,87 @@
+package edu.luc.etl.cs313.android.clickcounter.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Testcase superclass for the counter abstraction.
+ * 
+ * @author laufer
+ * @see http://xunitpatterns.com/Testcase%20Superclass.html
+ */
+public abstract class AbstractCounterTest {
+
+	protected Counter counter;
+
+	/**
+	 * Setter for dependency injection. Usually invoked by concrete testcase
+	 * subclass.
+	 * 
+	 * @param counter
+	 */
+	protected void setCounter(final Counter counter) {
+		this.counter = counter;
+	}
+
+	@Test
+	public void testPreconditions() {
+		// this counter has at least two different values
+		assertFalse(counter.isEmpty() && counter.isFull());
+	}
+
+	@Test
+	public void testIncrement() {
+		decrementIfFull();
+		assertFalse(counter.isFull());
+		final int v = counter.get();
+		counter.increment();
+		assertEquals(v + 1, counter.get());
+	}
+
+	@Test
+	public void testDecrement() {
+		incrementIfEmpty();
+		assertFalse(counter.isEmpty());
+		final int v = counter.get();
+		counter.decrement();
+		assertEquals(v - 1, counter.get());
+	}
+
+	@Test
+	public void testReset() {
+		incrementIfEmpty();
+		assertFalse(counter.isEmpty());
+		counter.reset();
+		assertTrue(counter.isEmpty());
+	}
+
+	@Test
+	public void testGet() {
+		// cannot check much other than consistency across invocations
+		final int v = counter.get();
+		assertEquals(v, counter.get());
+	}
+
+	@Test
+	public void testIsFull() {
+		decrementIfFull();
+		assertFalse(counter.isFull());
+	}
+
+	@Test
+	public void testIsEmpty() {
+		incrementIfEmpty();
+		assertFalse(counter.isEmpty());
+	}
+	
+	protected void decrementIfFull() {
+		if (counter.isFull()){ counter.decrement(); }
+	}
+	
+	protected void incrementIfEmpty() {
+		if (counter.isEmpty()) { counter.increment(); }
+	}
+}
