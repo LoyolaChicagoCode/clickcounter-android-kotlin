5868821f2f2e2ae19c0c35ec14bc927c038aea7a
Konstantin Laufer <laufer@cs.luc.edu>
2013-10-23 18:18:34 -0500

    fixed project layout for instrumentation tests

---
+++ ClickCounter/src/test/java/edu/luc/etl/cs313/android/clickcounter/android/ClickCounterActivityRobolectric.java
@@ -0,0 +1,36 @@
+package edu.luc.etl.cs313.android.clickcounter.android;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import android.util.Log;
+
+/**
+ * Concrete Robolectric test subclass.
+ *
+ * @author laufer
+ * @see http://pivotal.github.com/robolectric
+ */
+@Ignore
+@RunWith(RobolectricTestRunner.class)
+public class ClickCounterActivityRobolectric extends AbstractClickCounterActivityTest {
+
+	private static String TAG = "clickcounter-android-activity-robolectric";
+
+	private ClickCounterAdapter activity;
+
+	@Before
+	public void setUp() {
+		activity = new ClickCounterAdapter();
+		activity.onCreate(null);
+		activity.onStart();
+		Log.d(TAG, "setting up test...");
+	}
+
+	@Override
+	protected ClickCounterAdapter getActivity() {
+		return activity;
+	}
+}
